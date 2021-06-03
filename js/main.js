1c4e0876484125270b89b263826feea0e3477544
Bush Philip <fp.bush@gmail.com>
2021-06-03 21:35:58 +0300

    Вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ function getRandom (min, max) {
   if (min < 0 || max <= min) {
     return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
   }
-  let random = min + Math.random() * (max - min);
+  const random = min + Math.random() * (max - min);
   return random;
 }
-console.log(getRandom (-1, -10));
+getRandom (-1, -10);
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 function getRandomRound (min, max) {
   if (min < 0 || max <= min) {
-    return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".'
+    return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
   }
 
-  let random = min + Math.random() * (max - min + 1);
+  const random = min + Math.random() * (max - min + 1);
   return Math.floor(random);
 }
-console.log(getRandomRound (1, 9));
+getRandomRound (1, 9);
