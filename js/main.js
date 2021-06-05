148eed4ecd80cc9c42de96511fec07e3401f6fe3
Bush Philip <fp.bush@gmail.com>
2021-06-05 14:38:56 +0300

    Внес правки

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getRandom (min, max, exp) {
+const getRandom = (min, max, exp) => {
   if (min < 0 || max <= min) {
     return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
   }
+
   const random = min + Math.random() * (max - min);
   const expon = Math.pow(10, exp);
+
   return (Math.round(random * expon) / expon);
-}
+};
 getRandom (1, 7, 10);
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRandomRound (min, max) {
+const getRandomRound = (min, max) => {
   if (min < 0 || max <= min) {
     return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
   }
 
   const random = min + Math.random() * (max - min + 1);
+
   return Math.floor(random);
-}
+};
 getRandomRound (1, 9);
