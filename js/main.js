4be9c545f8eadf188f2c71649bd68358dd207cd5
Bush Philip <fp.bush@gmail.com>
2021-06-16 00:24:45 +0300

    Внес правки

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getRandom = (min, max, exp) => {
-  if (min < 0 || max <= min) {
-    return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
-  }
+const OFFER_COUNT = 10;
 
-  const random = min + Math.random() * (max - min);
-  const expon = Math.pow(10, exp);
+const minPrice = 1500;
+const maxPrice = 1000000;
+const minRooms = 1;
+const maxRooms = 25;
+const minGuests = 1;
+const maxGuests = 20;
+const latMin = 35.65000;
+const latMax = 35.70000;
+const lngMin = 139.70000;
+const lngMax = 139.80000;
 
-  return (Math.round(random * expon) / expon);
-};
-// getRandom (1, 7, 10);
-
-//Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getRandomRound = (min, max) => {
-  if (min < 0 || max <= min) {
-    return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
-  }
-
-  const random = min + Math.random() * (max - min + 1);
-
-  return Math.floor(random);
-};
-
-const TITLE = [
+const TITLES = [
   'testTitle01',
   'testTitle02',
   'testTitle03',
@@ -35,7 +24,7 @@ const TITLE = [
   'testTitle10',
 ];
 
-const TYPE = [
+const TYPES = [
   'palace',
   'flat',
   'house',
@@ -43,13 +32,7 @@ const TYPE = [
   'hotel',
 ];
 
-const CHECKIN = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const CHECKOUT = [
+const CHECK_IN_OUT_TIMES = [
   '12:00',
   '13:00',
   '14:00',
@@ -64,7 +47,7 @@ const FEATURES = [
   'conditioner',
 ];
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'testDescription1',
   'testDescription2',
   'testDescription3',
@@ -83,45 +66,65 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const OFFER_COUNT = 10;
+const getRandomFloat = (min, max, exp) => {
+  if (min < 0 || max <= min) {
+    return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
+  }
 
-const LAT =  getRandom(35.65000, 35.70000, 5);
-const LNG  =  getRandom(139.70000, 139.80000, 5);
+  const random = min + Math.random() * (max - min);
+  const expon = Math.pow(10, exp);
 
-const createOffer = () => {
-  let avatarIndex = getRandomRound (0, 10);
+  return (Math.round(random * expon) / expon);
+};
+
+const getRandomInt = (min, max) => {
+  if (min < 0 || max <= min) {
+    return 'Значение "от" не должно быть отрицательным. Значение "до" должно быть больше значения "от".';
+  }
+
+  const random = min + Math.random() * (max - min + 1);
+
+  return Math.floor(random);
+};
+
+const LAT = (min, max, exp) => getRandomFloat(min, max, exp);
+
+const LNG  = (min, max, exp) => getRandomFloat(min, max, exp);
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInt(0, elements.length - 1)];
+};
+
+const getRandomArraySlice = (elements) => {
+  const maxLengthOfSlice = elements.length;
+  const lengthOfSlice = getRandomInt(1, maxLengthOfSlice);
+  const startOfSlice = getRandomInt(1, lengthOfSlice);
+  return elements.slice(startOfSlice - 1, lengthOfSlice - 1);
+};
+
+const createOffer = (index) => {
+  let avatarIndex = index + 1;
   if (avatarIndex < 10) {
-    avatarIndex = '0' + avatarIndex;
+    avatarIndex ='0' + (index + 1);
+  } else {
+    String(avatarIndex);
   }
-  else {
-    avatarIndex = toString(avatarIndex);
-  }
-  const RandomAvatarIndex = avatarIndex;
-
-  const getRandomArrayElement = (elements) => {
-    return elements[getRandomRound(0, elements.length - 1)];
-  };
-
-  const getRandomArraySlice = (elements) => {
-    const maxLengthOfSlice = elements.length;
-    const lengthOfSlice = getRandomRound(1, maxLengthOfSlice);
-    const startOfSlice = getRandomRound(1, lengthOfSlice);
-    return elements.slice(startOfSlice - 1, lengthOfSlice - 1);
-  };
 
   return {
-    author: {avatar: `img/avatars/user${RandomAvatarIndex}.png`},
+    author: {
+      avatar: `img/avatars/user${avatarIndex}.png`,
+    },
     offer: {
-      title: getRandomArrayElement(TITLE),
-      addres: `${LAT}, ${LNG}`,
-      price: getRandomRound(1500, 1000000),
-      type: getRandomArrayElement(TYPE),
-      rooms: getRandomRound(1, 25),
-      guests: getRandomRound(1,20),
-      checkin: getRandomArrayElement(CHECKIN),
-      checkout: getRandomArrayElement(CHECKOUT),
+      title: getRandomArrayElement(TITLES),
+      address: `${LAT(latMin, latMax, 5)}, ${LNG(lngMin, lngMax, 5)}`,
+      price: getRandomInt(minPrice, maxPrice),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInt(minRooms, maxRooms),
+      guests: getRandomInt(minGuests, maxGuests),
+      checkin: getRandomArrayElement(CHECK_IN_OUT_TIMES),
+      checkout: getRandomArrayElement(CHECK_IN_OUT_TIMES),
       features: getRandomArraySlice(FEATURES),
-      description: getRandomArrayElement(DESCRIPTION),
+      description: getRandomArrayElement(DESCRIPTIONS),
       photos: getRandomArraySlice(PHOTOS),
       location: {
         lat: LAT,
@@ -131,4 +134,5 @@ const createOffer = () => {
   };
 };
 
-const similarOffer = new Array(OFFER_COUNT).fill(null).map(() => createOffer());
+const Offers = new Array(OFFER_COUNT).fill(null).map((item, index) => createOffer(index));
+
